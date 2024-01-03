5f4a88dff68d2ca6aac79490a2b36c9f80047f06
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-03 23:41:15 +0100

    Client setup with react

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <p className="lead">Welcome to subscription app with stripe, react and node</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
