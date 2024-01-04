42251d1a2ebe3186f6dc17a49e6eecb1fbe5d2d3
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-04 23:54:59 +0100

    Scaffold register page

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,14 @@
+const Register = () => {
+    return (
+        <div className="d-flex justify-content-center" style={{height: '80vh'}}>
+            <div className="container align-items-center d-flex">
+                <div className="row col-md-6 offset text-center">
+                    <h1 className="pt-5 fw-bold">Get Started...</h1>
+                    <p className="lead pb-4">Sign up free now.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
