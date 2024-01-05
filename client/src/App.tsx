aefd3cdae56a02462d83abec1ea72700884b72a1
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-05 21:00:15 +0100

    Implement navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />} />
