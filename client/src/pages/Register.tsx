92281f449f56d278087c216276263977057dd076
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-05 20:20:27 +0100

    contered elements

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,14 +1,18 @@
-const Register = () => {
-    return (
-        <div className="d-flex justify-content-center" style={{height: '80vh'}}>
-            <div className="container align-items-center d-flex">
-                <div className="row col-md-6 offset text-center">
-                    <h1 className="pt-5 fw-bold">Get Started...</h1>
-                    <p className="lead pb-4">Sign up free now.</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+import React from "react";
 
-export default Register
+const Register = () => {
+  return (
+    <div className="d-flex justify-content-center" style={{ height: "80vh" }}>
+      <div className="container align-items-center d-flex">
+        <div className="row col-md-6 offset-md-3 text-center">
+          <h1 className="pt-5 fw-bold">Let's Get Started</h1>
+          <p className="lead pb-4">
+            Sign up for free.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
