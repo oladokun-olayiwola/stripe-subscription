797b73b9da14b1e8739d20f6c9e236b4e4d6081a
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-06 21:20:03 +0100

    updt: added onChange

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { InputComponentProps } from '../IRegister'
 
-const Input = ({label, value, type = "text"}: InputComponentProps) => {
+const Input = ({label, value, type = "text", setValue}: InputComponentProps) => {
   return (
     <div className="input-group mb-3">
         <span className="input-group-text">
             {label}
         </span>
-        <input type={type} value ={value}className="form-control" />
+        <input type={type} value ={value} className="form-control" onChange={e => setValue(e.target.value)}/>
     </div>
   )
 }
