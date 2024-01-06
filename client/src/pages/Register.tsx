4a560256acaeba2450acd5bf09c13b3d5f6c32d2
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-06 20:28:40 +0100

    updt: Added Inputs fields for passwprd name and email

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../components/Input";
 
 const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div className="d-flex justify-content-center" style={{ height: "80vh" }}>
       <div className="container align-items-center d-flex">
@@ -9,6 +14,10 @@ const Register = () => {
           <p className="lead pb-4">
             Sign up for free.
           </p>
+
+          <Input label={"Name"} value={name} type="text" setValue={setName}/>
+          <Input label={"Email"} value={email} type="text" setValue={setEmail}/>
+          <Input label={"Password"} value={password} type="password" setValue={setPassword}/>
         </div>
       </div>
     </div>
