dbe60313537434c8b28d9f69663a7a3a55e29d0d
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-07 19:32:12 +0100

    updt: Modify custom input field

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { InputComponentProps } from '../IRegister'
+import { InputComponentProps } from '../Interfaces/IRegister'
 
-const Input = ({label, value, type = "text", setValue}: InputComponentProps) => {
-  return (
+const Input = ({label, value, type = "text", setValue}: InputComponentProps) => (
     <div className="input-group mb-3">
         <span className="input-group-text">
             {label}
         </span>
         <input type={type} value ={value} className="form-control" onChange={e => setValue(e.target.value)}/>
     </div>
-  )
-}
+)
 
 export default Input
