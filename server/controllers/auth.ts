c5b4e7305665ffa910d653718c2b5a93a21dbd9e
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-11 18:30:07 +0100

    updt: Add password add and register endpoint

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,20 +1,46 @@
 import { RequestHandler } from "express";
+import { UserCredentials } from "../interfaces/IRegister";
+import User from "../model/User"
+import { hashPassword, comparePassword } from "../helpers/auth";
 
-const register: RequestHandler = (req, res) => {
+const register: RequestHandler = async (req, res) => {
     const {name, email, password}: UserCredentials = req.body
-    if (!name) {
-        res.json({
-            error: true,
-            message: "Name is required",
-        })
+    try {
+        if (!name) {
+          return res.json({
+            error: "Name is required",
+          });
+        }
         if (!password || password.length < 6) {
-            res.json({
-                error: true,
-                message: "Password is required and should have  6 letters or more",
-            })
-    }
-    res.json({
-        error: false,
-        message: "User Registered"
-    })
-}}
+          return res.json({
+            error: "Password is required and should be 6 characters long",
+          });
+        }
+        const exist = await User.findOne({ email });
+        if (exist) {
+          return res.json({
+            error: "Email is taken",
+          });
+        }
+        // hash password
+        const hashedPassword = await hashPassword(password);
+    
+        try {
+          const user = await new User({
+            name,
+            email,
+            password: hashedPassword,
+          }).save();
+    
+          //   console.log(user);
+          const { password, ...rest } = user;
+          return res.json({
+            user: rest,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
