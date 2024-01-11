57d35832781bd2ec2c9614bb29458753d671fe5f
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-11 19:07:16 +0100

    updt: Implemented register route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { Register } from "../controllers/auth";
 const router = Router()
 
 router.get("/", (req, res)  => {
     res.send("Testing Endpoint.")
 })
 
+router.post("/api/register", Register)
 export default router
