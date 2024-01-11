72f2c25873c29986c3131657d5889c95818a10eb
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-11 19:04:41 +0100

    Updt: Refactor register controller

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,46 +1,45 @@
-import { RequestHandler } from "express";
-import { UserCredentials } from "../interfaces/IRegister";
-import User from "../model/User"
-import { hashPassword, comparePassword } from "../helpers/auth";
+import { RequestHandler } from 'express';
+import { UserCredentials } from '../interfaces/IRegister';
+import User from '../model/User';
+import { hashPassword, comparePassword } from '../helpers/auth';
 
-const register: RequestHandler = async (req, res) => {
-    const {name, email, password}: UserCredentials = req.body
-    try {
-        if (!name) {
-          return res.json({
-            error: "Name is required",
-          });
-        }
-        if (!password || password.length < 6) {
-          return res.json({
-            error: "Password is required and should be 6 characters long",
-          });
-        }
-        const exist = await User.findOne({ email });
-        if (exist) {
-          return res.json({
-            error: "Email is taken",
-          });
-        }
-        // hash password
-        const hashedPassword = await hashPassword(password);
-    
-        try {
-          const user = await new User({
-            name,
-            email,
-            password: hashedPassword,
-          }).save();
-    
-          //   console.log(user);
-          const { password, ...rest } = user;
-          return res.json({
-            user: rest,
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
+export const Register: RequestHandler = async (req, res) => {
+  const { name, email, password }: UserCredentials = req.body;  
+  try {
+    if (!name) {
+      return res.json({
+        error: 'Name is required',
+      });
+    }
+    if (!password || password.length < 6) {
+      return res.json({
+        error: 'Password is required and should be 6 characters long',
+      });
+    }
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: true,
+        message: " User already exists"
+      });
+    }
+
+    // hash password
+    const hashedPassword = await hashPassword(password);    
+
+    const user = await User.create({ name, email, password: hashedPassword });
+    const { password: newPass, ...rest } = user;
+    return res.status(201).json({
+      error: false,
+      message: 'User created successfully',
+      user: rest,
+    });
+
+  } catch (err) {
+    return res.json({
+      error: true,
+      message: 'Failed to create user',
+      err,
+    });
+  }
+};
