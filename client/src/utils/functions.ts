1e042a1f723b04b29e479f2f362624b06b27c094
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2024-01-28 21:29:53 +0100

    Protected route...

--- client/src/utils/functions.ts
+++ client/src/utils/functions.ts
@@ -1,9 +1,14 @@
-import { log } from "console"
-import { Request } from "express"
+import { ReactComponentElement, ReactElement, useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { UserContext } from "../context"
+
 export const isAuthenticated = () => {
     const token = localStorage.getItem("Token")
     if(token) return token
-    log(token)
     return ""
 }
 
+
+export const ProtectedRoute = ({ element }: any) => {
+const {token} = useContext(UserContext)
+}; 
